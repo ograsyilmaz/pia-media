4ab0c6af0a2df7ad83395d9fb87274b2eb48c562
yilmazogras <yilmazogras@gmail.com>
2017-03-10 19:12:40 +0300

    asd

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 
 
+function showAlert(divid,mesaj) {
+    var ikincidiv= divid+"2";
+    if($('#'+divid).find('div'+'#'+ikincidiv).length==0){
+        $('#'+divid).append('<div class="alert" id="'+ikincidiv+'" ><a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a><img src="img/commed.png" alt="">'+mesaj+'</div>');
 
-function showAlert(divid,icdivid,mesaj) {
-    if($('#'+divid).find('div'+'#'+icdivid).length==0){
-        $('#'+divid).append('<div class="alert" id="'+icdivid+'" ><a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a><img src="img/commed.png" alt="">'+mesaj+'</div>');
-
-}
-$(divid).css("display", "");
+    }
+    $(divid).css("display", "");
 }
