6b4191b5e86548a7c6313eac64be5f2a9dc36b60
yilmazogras <yilmazogras@gmail.com>
2017-03-10 19:47:42 +0300

    asdasda

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@
 function showAlert(divid,mesaj) {
     var ikincidiv= divid+"2";
     if($('#'+divid).find('div'+'#'+ikincidiv).length==0){
-        $('#'+divid).append('<div class="alert" id="'+ikincidiv+'" ><a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a><img src="img/commed.png" alt="">'+mesaj+'</div>');
+        $('#'+divid).append('<div class="alert" id="'+ikincidiv+'" ><a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a><img src="img/commed.png" alt=""> '+mesaj+'</div>');
 
     }
     $(divid).css("display", "");
